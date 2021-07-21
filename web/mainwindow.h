fe14d3f397634436e53b561df2cbe7b7d6a50da2
idcpj <260083304@qq.com>
2021-07-21 23:30:36 +0800

    qt clearn

--- web/mainwindow.h
+++ web/mainwindow.h
@@ -1,7 +1,9 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QLineEdit>
 #include <QMainWindow>
+#include <QWebEngineView>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +19,17 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QWebEngineView *view;
+    QLineEdit *lineEdit;
+    int progress=0;
+
+
+private slots:
+    void loadFinished(bool);
+    void changeLocation();     // 改变路径
+    void setProgress(int);     // 更新进度
+    void adjustTitle();        // 更新标题显示
+    void finishLoading(bool);  // 加载完成后进行处理
+
 };
 #endif // MAINWINDOW_H
